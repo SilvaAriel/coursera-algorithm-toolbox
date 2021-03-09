4a3903a3c4b324a483f1ea7b49c000b4f1f88341
Ariel silva <arielsilva@pop-os.localdomain>
2021-03-09 19:16:23 -0300

    Implement Week 2 algorithm 1, 2, 3

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,14 @@
 # Uses python3
+
 def calc_fib(n):
     if (n <= 1):
         return n
+    
+    if (n not in cache):
+      cache[n] = calc_fib(n - 1) + calc_fib(n - 2)
+      return cache[n]
+    return cache[n]
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
-
+cache = {}
 n = int(input())
 print(calc_fib(n))
--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,19 +1,18 @@
 # Uses python3
 import sys
 
-def get_fibonacci_last_digit_naive(n):
-    if n <= 1:
+def get_fibonacci_last_digit_better(n):
+    if (n <= 1):
         return n
 
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % 10
+    if n not in cache:
+        cache[n] = (get_fibonacci_last_digit_better(n - 1) + 
+                    get_fibonacci_last_digit_better(n - 2)) % 10
+        return cache[n]
+    return cache[n]
 
 if __name__ == '__main__':
+    cache = {}
     input = sys.stdin.read()
     n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    print(get_fibonacci_last_digit_better(n))
