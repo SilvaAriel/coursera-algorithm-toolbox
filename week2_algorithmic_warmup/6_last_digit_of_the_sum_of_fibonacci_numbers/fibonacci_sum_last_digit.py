99ca6a46909fd3deccee5a080d702625c1323b1d
Ariel Abreu Silva <ariel120292@gmail.com>
2021-03-12 19:38:11 +0000

    Tidy up 6_last_digit_of_the_sum_of_fibonacci_numbers

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,37 +1,35 @@
 # Uses python3
 import sys
 
+
 def fib(n):
     if n <= 1:
         return n
-    previous  = 0
-    current   = 1
-    sum       = 1
+    previous = 0
+    current = 1
+    sum = 1
     for _ in range(n - 1):
         previous, current = current, previous + current
         sum += current
     return sum
 
+
 def pisano_period(m):
-  previous, current = 0, 1
-  for i in range (0, m * m):
-    previous, current = current, (previous + current) % m
-    if previous == 0 and current == 1:
-      return i + 1
+    previous, current = 0, 1
+    for i in range(0, m * m):
+        previous, current = current, (previous + current) % m
+        if previous == 0 and current == 1:
+            return i + 1
+
 
 def fib_modulo(n, m):
-  m_pisano = pisano_period(m)
-  print(f'm_pisano: {m_pisano}')
-  n_modulo_m_pisano = n % m_pisano
-  print(f'n_modulo_m_pisano: {n_modulo_m_pisano}')
-  new_fib = fib(n_modulo_m_pisano)
-  print(f'new_fib: {new_fib}')
-  return new_fib % m
+    m_pisano = pisano_period(m)
+    n_modulo_m_pisano = n % m_pisano
+    new_fib = fib(n_modulo_m_pisano)
+    return new_fib % m
+
 
 if __name__ == '__main__':
-    # input = sys.stdin.read();
-    # n, m = map(int, input.split())
-
-    n, m = 100, 10
-
-    print(fib_modulo(n,m))
+    input = sys.stdin.read()
+    n = int(input)
+    print(fib_modulo(n, 10))
