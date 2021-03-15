9ad8a4f6fbf604489fe8927591763106abeb83b9
Ariel Abreu Silva <ariel120292@gmail.com>
2021-03-15 19:15:35 +0000

    Add Pisano Period to algorithm 7 and 8 from Week 2

--- week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
+++ week2_algorithmic_warmup/7_last_digit_of_the_sum_of_fibonacci_numbers_again/fibonacci_partial_sum.py
@@ -12,7 +12,20 @@ def fib(b, n):
         previous, current = current, previous + current
     return sum % 10
 
+def pisano_period(m):
+  previous, current = 0, 1
+  for i in range (0, m * m):
+    previous, current = current, (previous + current) % m
+    if previous == 0 and current == 1:
+      return i + 1
+
+def fib_modulo(b, n):
+  m_pisano = pisano_period(10)
+  n_modulo_m_pisano = n % m_pisano
+  new_fib = fib(b, n_modulo_m_pisano)
+  return new_fib % 10
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     b, n = map(int, input.split())
-    print(fib(b, n))
+    print(fib_modulo(b, n))
--- week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
+++ week2_algorithmic_warmup/8_last_digit_of_the_sum_of_squares_of_fibonacci_numbers/fibonacci_sum_squares.py
@@ -1,7 +1,7 @@
 # Uses python3
 from sys import stdin
 
-def fibonacci_sum_squares_naive(n):
+def fib(n):
     if n <= 1:
         return n
 
@@ -15,6 +15,19 @@ def fibonacci_sum_squares_naive(n):
 
     return sum % 10
 
+def pisano_period(m):
+  previous, current = 0, 1
+  for i in range (0, m * m):
+    previous, current = current, (previous + current) % m
+    if previous == 0 and current == 1:
+      return i + 1
+
+def fib_modulo(n):
+  m_pisano = pisano_period(10)
+  n_modulo_m_pisano = n % m_pisano
+  new_fib = fib(n_modulo_m_pisano)
+  return new_fib % 10
+
 if __name__ == '__main__':
     n = int(stdin.read())
-    print(fibonacci_sum_squares_naive(n))
+    print(fib_modulo(n))
